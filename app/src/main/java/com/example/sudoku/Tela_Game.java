e06e07969dd4db502a4e735bf87776dc7e87ddde
Nathalya Silva <nathalya.gsn@gmail.com>
2025-07-23 10:21:46 -0300

    backup

---
+++ app/src/main/java/com/example/sudoku/Tela_Game.java
@@ -0,0 +1,31 @@
+package com.example.sudoku;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Tela_Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tela_game);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void voltar(View view){
+        Intent it_Tela_Inicial = new Intent(this, MainActivity.class);
+        startActivity(it_Tela_Inicial);
+    }
+}
